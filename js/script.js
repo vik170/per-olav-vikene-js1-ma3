428c0c70a8e41af1fc0855fe69b266da25f15fb4
vik170 <per.vikene@gmail.com>
2021-02-06 18:02:09 +0100

    Added API url

--- js/script.js
+++ js/script.js
@@ -10,7 +10,5 @@ const division = (a, b) =>  a - b;
 
 //Question 2
 
-
-for (var i = 0; i < i.lenght; i++) {
-
-}
+const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
+const resultsContainer = document.querySelector(".games");
