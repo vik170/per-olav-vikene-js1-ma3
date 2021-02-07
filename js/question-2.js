c7206d5169477071ba5c67036e58e53b4dc17d02
vik170 <per.vikene@gmail.com>
2021-02-07 14:36:27 +0100

    Added css

--- js/question-2.js
+++ js/question-2.js
@@ -1,6 +1,26 @@
 //Question 2
 
 const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
-const resultsContainer = document.querySelector(".games");
+const container = document.querySelector(".games");
+
+async function getGames() {
+	const response = await fetch(url);
+
+	const results = await response.json();
+
+	const games = results.results;
+
+	for (i = 0; i < games.length; i++) {
+		if (i === 8) {
+			break;
+		}
+		container.innerHTML += `<div class="games">
+									<ul>
+										<li>${games[i].name} Rating: ${games[i].rating}</li>
+									</ul>
+								</div>`;
+    }
+}
 
 
+getGames();
